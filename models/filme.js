07ece7a108ae550c7f0f629cf160a7e0f887ba16
Lua-programmer <[luanamelissaprogrammer@gmail.com]>
2021-10-13 17:27:13 -0300

    models/filme.js

---
+++ models/filme.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose'); //Importando o mongoose
+
+const FilmeModel = new mongoose.Schema({
+    nome: { type: String, required: true},
+    genero: { type: String, required: true},
+    nota: { type: Number, required: true},
+    imagem: { type: String, required: true},
+    dataCriacao: { type: Date, default: Date.now}
+})
+
+const Filme = mongoose.model("filmes, FilmeModel");
+
+module.exports = Filme;
